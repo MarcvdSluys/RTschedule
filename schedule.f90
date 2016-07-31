b987abbe1ed6bbfb08991d950fe5fdc90b655214
MvdS <git@vandersluys.nl>
2016-07-31 15:48:06 +0200

    Add subroutine schedule_RM() to make an RM schedule
    * Add subroutine schedule_RM() to make an RM schedule:
      * heavily borrowed from LLF - merge?

--- schedule.f90
+++ schedule.f90
@@ -1,10 +1,12 @@
 !***********************************************************************************************************************************
 program schedule
+  use SUFR_kinds, only: double
   use SUFR_constants, only: set_SUFR_constants
   
   implicit none
   integer, parameter :: nProcMax=19  ! Maximum number of processes to expect
   integer :: np, time, ti(nProcMax),ci(nProcMax),di(nProcMax),pi(nProcMax)
+  real(double) :: load
   character :: name(nProcMax)*(9)
   
   ! Initialise libSUFR:
@@ -14,11 +16,15 @@ program schedule
   call read_input_file(nProcMax, name, ti,ci,di,pi, np,time)
   
   ! Print some basic data about the system:
-  call print_system_data(np,time, name,ti,ci,di,pi)
+  call print_system_data(np,time, name,ti,ci,di,pi, load)
+  
+  
+  ! Create an RM schedule:
+  call schedule_RM(np,time, name, ti,ci,di,pi, load)
   
   
   ! Create a LLF schedule:
-  call schedule_LLF(np,time, name, ti,ci,di,pi)
+  !call schedule_LLF(np,time, name, ti,ci,di,pi)
   
   write(*,*)
 end program schedule
@@ -71,7 +77,7 @@ end subroutine read_input_file
 
 
 !***********************************************************************************************************************************
-subroutine print_system_data(np,time, name,ti,ci,di,pi)
+subroutine print_system_data(np,time, name,ti,ci,di,pi, load)
   use SUFR_kinds, only: double
   use SUFR_text, only: d2s
   use SUFR_numerics, only: gcd,lcm
@@ -80,8 +86,9 @@ subroutine print_system_data(np,time, name,ti,ci,di,pi)
   integer, intent(in) :: np, ti(np),ci(np),di(np),pi(np)
   integer, intent(inout) :: time
   character, intent(in) :: name(np)*(9)
+  real(double), intent(out) :: load
   integer :: pr, optts,majFr
-  real(double) :: frac,load
+  real(double) :: frac
   
   ! Print task list:
   write(*,'(2x,A)') 'Task list:'
@@ -112,9 +119,9 @@ subroutine print_system_data(np,time, name,ti,ci,di,pi)
   end do
   write(*,'(A)') ' = '//d2s(load,4)
   if(load.gt.1.d0) then
-     write(*,'(A)') '  The system is NOT schedulable indefinately... :-('
+     write(*,'(A)') '  The task list is NOT schedulable indefinately... :-('
   else
-     write(*,'(A)') '  The system is SCHEDULABLE! :-)'
+     write(*,'(A)') '  The task list is SCHEDULABLE in principle (feasible) :-)'
   end if
   write(*,*)
   
@@ -126,6 +133,163 @@ subroutine print_system_data(np,time, name,ti,ci,di,pi)
 
 end subroutine print_system_data
 !***********************************************************************************************************************************
+
+
+!***********************************************************************************************************************************
+subroutine schedule_RM(np,time, name, ti,ci,di,pi, load)
+  use SUFR_kinds, only: double
+  use SUFR_constants, only: set_SUFR_constants
+  use SUFR_sorting, only: sorted_index_list
+  use SUFR_text, only: d2s
+  
+  implicit none
+  integer, intent(in) :: np,time, ti(np),ci(np),di(np),pi(np)
+  real(double), intent(in) :: load
+  character, intent(in) :: name(np)*(9)
+  integer, allocatable :: run(:), ccs(:,:)
+  integer :: indx(np), it,pr, prio(np),cc(np),tte(np), ri,ro, nSwitch,nMiss !Nopts
+  real(double) :: maxLoad
+  character :: ccpr*(9),priopr*(9)
+  
+  ! Determine priorities:
+  call sorted_index_list(dble(pi), indx)
+  
+  do pr=1,np
+     prio(indx(pr)) = pr
+  end do
+  
+  ! Print task list with RM priorities:
+  write(*,'(2x,A)') 'RM priorities:'
+  write(*,'(2x,9A5)') 'Name', 'ti','ci','di','pi','prio'
+  do pr=1,np
+     write(*,'(2x,A5, 9I5)') trim(name(pr)), ti(pr),ci(pr),di(pr),pi(pr), prio(pr)
+  end do
+  write(*,*)
+  
+  ! RM schedulability test:
+  maxLoad = np * (2.d0**(1.d0/dble(np)) - 1.d0)
+  write(*,'(A,I0,A)') '  RM schedulability test for n=',np,':  SUM Ci/Pi <= n (2^(1/n) - 1):'
+  write(*,'(A)', advance='no') '    '//d2s(load,3)//' <= '//d2s(maxLoad,3)
+  if(load.le.maxLoad) then
+     write(*,'(A)') ', so task set is GUARANTEED to be schedulable with RM.'
+  else if(load.le.1.d0) then
+     write(*,'(A)') ', so task set is NOT guaranteed to be schedulable with RM.'
+  else
+     write(*,'(A)') ', so task set is NOT SCHEDULABLE indefinately.'
+  end if
+  
+  
+  ! Start scheduling
+  ro=1; tte=0
+  allocate(ccs(np,time), run(time));  ccs = 0
+  nSwitch=0; nMiss=0
+  
+  ! Initial computation times:
+  cc=0
+  do pr=1,np
+     if(ti(pr).eq.0) then
+        cc(pr) = ci(pr)
+     end if
+  end do
+  
+  do it=1,time
+     
+     ! Save cc for later use:
+     ccs(1:np,it) = cc(1:np)
+     
+
+     ! Determine running task:
+     ri = minloc(prio(1:np), 1, cc(1:np).gt.0)  ! Running task: minimum prio and cc>0
+     if(ri*ro.ne.0) then
+        if(it.ne.1 .and. ri.ne.ro .and. cc(ro).gt.0 .and. prio(ro).le.prio(ri)) ri = ro  ! Keep old task running if prios are equal
+     end if
+     run(it) = ri  ! Save for later use
+     
+     ! Print timestamp:
+     write(*,'(2x,I0,A,I0,T11,A)', advance='no') it-1,'-',it, ''
+     
+     ! Print detailed data:
+     do pr=1,np
+        
+        write(ccpr,'(I0)') cc(pr)
+        write(priopr,'(I0)') prio(pr)
+        if(pr.eq.ri) then
+           ccpr = '_'//trim(ccpr)//'_'
+        else
+           ccpr = ' '//trim(ccpr)
+        end if
+        
+        !write(*,'(5x,A4)', advance='no') priopr
+        write(*,'(2x,A4)', advance='no') ccpr
+        write(*,'(2x,I4)', advance='no') tte(pr)
+        
+
+        ! Label new event:
+        if(it.ge.ti(pr) .and. tte(pr).eq.0) then  ! New event
+           write(*,'(1x,A)', advance='no') 'e'
+        else
+           write(*,'(1x,A)', advance='no') ' '
+        end if
+        
+
+     end do  ! pr
+     
+     write(*,'(5x,2I4)', advance='no') ri,ro
+     
+     
+     ! Label task switch:
+     if(it.gt.1 .and. ri.ne.ro) then
+        write(*,'(2x,A)', advance='no') 'switch'
+        nSwitch = nSwitch + 1
+        if(ro.ne.0) then
+           if(cc(ro).gt.0) then
+              write(*,'(A,I0,A)', advance='no') ' (task '//trim(name(ro))//': ',cc(ro), '>)'
+           else
+              write(*,'(A)', advance='no') ' (task '//trim(name(ro))//' done)'
+           end if
+        end if
+     end if
+     
+
+     
+     ! Run the task; decrease its cc:
+     if(ri.gt.0) cc(ri) = cc(ri) - 1
+     
+     
+     ! New event:
+     do pr=1,np
+        tte(pr) = mod( ti(pr)+di(pr)-it + pi(pr)*1000, pi(pr))  ! Time to next deadline
+        if(tte(pr).eq.0) then        ! New event occurs
+           cc(pr) = ci(pr)           ! Reset the computation time 
+        end if
+     end do
+     
+     ro = ri
+     write(*,*)
+  end do  ! it
+  
+
+
+  ! 'Plot' an ascii scheduler:
+  call plot_ascii_scheduler(np,time, name,ti,pi,di, run, .false.)  ! Detail: .true./.false.
+  call plot_ascii_scheduler(np,time, name,ti,pi,di, run, .true.)  ! Detail: .true./.false.
+  
+  
+  ! Graphical plot:
+  call plot_scheduler(np,time, name,ti,pi,di, ccs,run)
+  
+  
+  ! Report on missed deadlines:
+  if(nMiss.eq.0) then
+     write(*,'(/,A,I0,A)') '  No deadlines were missed: the system can be scheduled for ', time, ' time units.'
+  else
+     write(*,'(/,2x,I0,A,I0,A)') nMiss, ' DEADLINES HAVE BEEN MISSED in ', time, ' time units.'
+  end if
+  write(*,'(2x,I0,A)') nSwitch, ' task switches ('//d2s(dble(time)/dble(nSwitch+1),2)//' time units per run).'
+  
+end subroutine schedule_RM
+!***********************************************************************************************************************************
+
 
 
 !***********************************************************************************************************************************
@@ -203,13 +367,14 @@ subroutine schedule_LLF(np,time, name, ti,ci,di,pi)
         !write(*,'(A2)', advance='no') trim(ccpr)
         !write(*,'(A2)', advance='no') trim(ttepr)
         
+        ! Label new event:
         if(it.ge.ti(pr) .and. tte(pr).eq.0) then  ! New event
            write(*,'(1x,A)', advance='no') 'e'
         else
            write(*,'(1x,A)', advance='no') ' '
         end if
         
-     end do
+     end do  ! pr
      
      
      ! Print which task is running + its laxity:
@@ -218,6 +383,8 @@ subroutine schedule_LLF(np,time, name, ti,ci,di,pi)
      else
         write(*,'(5x,3(A,I0))', advance='no') 'run: ',ri, ',  lax: ', li(ri), ',  cpu: ', cc(ri)
      end if
+     
+     ! Label task switch:
      if(it.gt.1 .and. ri.ne.ro) then
         write(*,'(2x,A)', advance='no') 'switch'
         nSwitch = nSwitch + 1
@@ -241,7 +408,7 @@ subroutine schedule_LLF(np,time, name, ti,ci,di,pi)
      
      
      
-     ! Current running job is ci, all other laxities decrease:
+     ! Run job: current job is ri, so its cc decreases; all other laxities decrease:
      do pr=1,np
         if(pr.eq.ri) then
            cc(pr) = cc(pr) - 1
@@ -270,9 +437,12 @@ subroutine schedule_LLF(np,time, name, ti,ci,di,pi)
   call plot_ascii_scheduler(np,time, name,ti,pi,di, run, .false.)  ! Detail: .true./.false.
   call plot_ascii_scheduler(np,time, name,ti,pi,di, run, .true.)  ! Detail: .true./.false.
   
+  
   ! Graphical plot:
   call plot_scheduler(np,time, name,ti,pi,di, ccs,run)
   
+  
+  ! Report on missed deadlines:
   if(nMiss.eq.0) then
      write(*,'(/,A,I0,A)') '  No deadlines were missed: the system can be scheduled for ', time, ' time units.'
   else
