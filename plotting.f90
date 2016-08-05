98a0d059895696a8ee816331096f81f6b1cda88b
MvdS <git@vandersluys.nl>
2016-08-05 15:28:53 +0200

    Add eps, pdf, svg output;  add variable sclType to set margin scale type;  scale line width

--- plotting.f90
+++ plotting.f90
@@ -3,28 +3,44 @@
 
 subroutine plot_scheduler(sched, np,time, name,ti,pi,di, ccs,run, fileBaseName)
   use SUFR_kinds, only: double
+  use SUFR_system, only: quit_program_error
   use plplot, only: plsdev, plsfnam, plbox, plmtex,plfill,plptex, plpoin
   
   implicit none
   integer, intent(in) :: np,time, ti(np),pi(np),di(np), ccs(np,time),run(time)
   character, intent(in) :: sched*(9), name(np)*(9), fileBaseName*(99)
-  integer :: it, pr, xSize,ySize
-  real(double) :: xMarg1,xMarg2,yMarg1,yMarg2, schFac
-  character :: tmpStr*(9), plFileName*(99)
+  integer :: it, pr, xSize,ySize, sclType
+  real(double) :: xMarg1,xMarg2,yMarg1,yMarg2, sclFac
+  character :: tmpStr*(9), plFileName*(99), plotType*(19)
   logical :: clr
   
   ! B/W or colour?
   clr = .true.   ! Create a colour image
   clr = .false.  ! Create a black-and-white image
-  
+  plotType = 'png'  ! Plot type: png, pdf, svg, eps
+  sclType = 2     ! Margin scale type 1, 2
   
   ! time x np boxes of 50px, left/bottom margin: 70px, top/right: 20px:
-  call pl_square_grid(time,np, 50, 70,20, xSize,ySize, xMarg1,xMarg2, yMarg1,yMarg2, schFac)
+  call pl_square_grid(time,np, 50, 50,12, sclType, xSize,ySize, xMarg1,xMarg2, yMarg1,yMarg2, sclFac)
   
   
-  plFileName = trim(fileBaseName)//'_schedule_'//trim(sched)//'.png'  ! Add to the base name of the input file
+  select case(trim(plotType))
+  case('eps')  ! MS Word can't do pdf or svg(!) - turns out it has difficulties with eps as well...
+     plFileName = trim(fileBaseName)//'_schedule_'//trim(sched)//'.eps'  ! Add to the base name of the input file
+     call plsdev('epscairo')                         ! Set plotting device: eps
+  case('pdf')
+     plFileName = trim(fileBaseName)//'_schedule_'//trim(sched)//'.pdf'  ! Add to the base name of the input file
+     call plsdev('pdfcairo')                         ! Set plotting device: pdf
+  case('png')
+     plFileName = trim(fileBaseName)//'_schedule_'//trim(sched)//'.png'  ! Add to the base name of the input file
+     call plsdev('pngcairo')                         ! Set plotting device: png
+  case('svg')
+     plFileName = trim(fileBaseName)//'_schedule_'//trim(sched)//'.svg'  ! Add to the base name of the input file
+     call plsdev('svgcairo')                         ! Set plotting device: svg
+  case default
+     call quit_program_error('Plot type unknown: '//trim(plotType)//' - use one of png, pdf, svg', 1)
+  end select
   call plsfnam(trim(plFileName))                  ! Set file name
-  call plsdev('pngcairo')                         ! Set plotting device: png
   
   
   call plspage(0.d0,0.d0, xSize,ySize, 0,0)       ! Set page size: dpi, size, offset (px/mm)
@@ -35,7 +51,7 @@ subroutine plot_scheduler(sched, np,time, name,ti,pi,di, ccs,run, fileBaseName)
   call plvpor(xMarg1,xMarg2, yMarg1,yMarg2)       ! Set view port in plot window
   call plwind(0,dble(time), dble(np),0)           ! Set view port in world coordinates
   
-  call plwidth(2.d0)                              ! Thick lines
+  call plwidth(1.d0*sclFac)                       ! Normal line width
   call pllsty(1)                                  ! Full lines
   
   do it=1,time
@@ -67,24 +83,24 @@ subroutine plot_scheduler(sched, np,time, name,ti,pi,di, ccs,run, fileBaseName)
   
   
   if(clr) call plcol0(2)                        ! Red arrows, dots and crosses
-  call plwidth(2.5d0)                          ! Thicker lines
+  call plwidth(1.5d0*sclFac)                    ! Thicker lines
   do it=0,time
      
      do pr=1,np
         
         ! Mark event (up arrow, as in SimSo - len 0.25, angle 25d):
-        if( mod( ti(pr)-it + pi(pr)*1000, pi(pr)).eq.0 )  call plarrow(2,  dble([it,it]), dble([pr,pr-1]), 0.25d0*schFac, 25.d0)
+        if( mod( ti(pr)-it + pi(pr)*1000, pi(pr)).eq.0 )  call plarrow(2,  dble([it,it]), dble([pr,pr-1]), 0.25d0*sclFac, 25.d0)
         
         ! Mark deadline (down arrow, as in SimSo):
         if( mod( ti(pr)+di(pr)-it + pi(pr)*1000, pi(pr)).eq.0 )  then
-           call plarrow( 2, dble([it,it]), dble([pr-1,pr]), 0.25d0*schFac, 25.d0)  ! len = 0.25, angle 25d
+           call plarrow( 2, dble([it,it]), dble([pr-1,pr]), 0.25d0*sclFac, 25.d0)  ! len = 0.25, angle 25d
            
            ! Mark missed deadlines:
            if(it.gt.0) then
               if(ccs(pr,it).gt.1) then  ! Process pr misses a deadline at t=it
-                 call plssym(10.d0, schFac)  ! Huge symbols
+                 call plssym(10.d0, sclFac)  ! Huge symbols
                  call plpoin(dble([it]), dble([pr])-0.5d0, 5)  ! Cross
-                 call plssym(5.d0, schFac)  ! Default symbol size
+                 call plssym(5.d0, sclFac)  ! Default symbol size
               end if
            end if
            
@@ -96,16 +112,16 @@ subroutine plot_scheduler(sched, np,time, name,ti,pi,di, ccs,run, fileBaseName)
      if(it.gt.1) then  ! Can't have a task switch in timeslise 0-1
         if(run(it).ne.run(it-1) .and. run(it-1).ne.0) then  ! There must be a task switch
            if(ccs(run(it-1),it).le.0) then  ! No CPU time left for the old task
-              call plssym(7.d0, schFac)  ! Larger symbols
+              call plssym(7.d0, sclFac)  ! Larger symbols
               call plpoin([dble(it-1)], [dble(run(it-1))], 17)
-              call plssym(5.d0, schFac)  ! Default symbol size
+              call plssym(5.d0, sclFac)  ! Default symbol size
            end if
         end if
      end if
      
   end do  ! it
   
-  call plwidth(2.d0)                          ! Thick lines
+  call plwidth(1.d0*sclFac)                          ! Normal line width
   
   
   
@@ -113,7 +129,7 @@ subroutine plot_scheduler(sched, np,time, name,ti,pi,di, ccs,run, fileBaseName)
   ! Print axis labels:
   call plcol0(1)                                   ! Black text
   call plmtex('B', 3.5d0, 0.5d0,0.5d0, 'Time')     ! Plot label for horizontal axis
-  call plmtex('L', 3.5d0, 0.5d0,0.5d0, 'Process')  ! Plot label for vertical axis
+  call plmtex('L', 3.5d0, 0.5d0,0.5d0, 'Task')  ! Plot label for vertical axis
   
   
   ! Print the task names:
@@ -137,29 +153,27 @@ end subroutine plot_scheduler
 !! \param marg1     Pixels for left/bottom margin
 !! \param marg2     Pixels for right/top margin 
 
-subroutine pl_square_grid(nx,ny, boxSize, marg1,marg2, xSize,ySize, xMarg1,xMarg2, yMarg1,yMarg2, schFac)
+subroutine pl_square_grid(nx,ny, boxSize, marg1,marg2, sclType, xSize,ySize, xMarg1,xMarg2, yMarg1,yMarg2, sclFac)
   use SUFR_system, only: quit_program_error
   use plplot, only: plflt
   
   implicit none
-  integer, intent(in) :: nx,ny, boxSize, marg1,marg2
+  integer, intent(in) :: nx,ny, boxSize, marg1,marg2, sclType
   integer, intent(out) :: xSize,ySize
-  real(plflt), intent(out) :: xMarg1,xMarg2, yMarg1,yMarg2
-  integer :: method, xPlBox
-  real(plflt) :: rat, schFac
+  real(plflt), intent(out) :: xMarg1,xMarg2, yMarg1,yMarg2, sclFac
+  integer :: xPlBox
+  real(plflt) :: rat
   
+  ! sclType:
   ! There are (at least) two ways to scale the image and get a (roughly) square grid:
   !   1) the image always gets the same size, if the image becomes very large and is
   !      used in a document, it will be shrunk and become very small
   !   2) the font size scales with the image width, becomes larger for larger images,
   !      so that the absolute margins need to grow
   
-  method = 1
+  sclFac = 1.d0
   
-  
-  schFac = 1.d0
-     
-  select case(method)
+  select case(sclType)
   case(1)
      xSize = 1000
      xPlBox = xSize - (marg1 + marg2)  ! Size of the plot box (excluding margins) in pixels
@@ -169,22 +183,23 @@ subroutine pl_square_grid(nx,ny, boxSize, marg1,marg2, xSize,ySize, xMarg1,xMarg
      
   case(2)
      xSize = nx * boxSize + marg1 + marg2  ! Image size in pixels
-     schFac = dble(xSize)/1000.d0                           ! Font size -> margin scales with image width
-     xSize = nx * boxSize + nint((marg1 + marg2) * schFac)  ! Image size in pixels
+     sclFac = dble(xSize)/1000.d0                           ! Font size -> margin scales with image width
+     xSize = nx * boxSize + nint((marg1 + marg2) * sclFac)  ! Image size in pixels
      
-     ySize = ny * boxSize + nint((marg1 + marg2) * schFac) 
+     ySize = ny * boxSize + nint((marg1 + marg2) * sclFac) 
      
   case default
-     call quit_program_error('pl_square_grid():  Unknown method', 1)
+     call quit_program_error('pl_square_grid():  Unknown scale type', 1)
   end select
   
   
   ! Compute the relative margins:
-  xMarg1 =        dble(marg1)/dble(xSize) * schFac  ! Fractional margins for plvpor()
-  xMarg2 = 1.d0 - dble(marg2)/dble(xSize) * schFac
-  yMarg1 =        dble(marg1)/dble(ySize) * schFac  ! Fractional margins for plvpor()
-  yMarg2 = 1.d0 - dble(marg2)/dble(ySize) * schFac
+  xMarg1 =        dble(marg1)/dble(xSize) * sclFac  ! Fractional margins for plvpor()
+  xMarg2 = 1.d0 - dble(marg2)/dble(xSize) * sclFac
+  yMarg1 =        dble(marg1)/dble(ySize) * sclFac  ! Fractional margins for plvpor()
+  yMarg2 = 1.d0 - dble(marg2)/dble(ySize) * sclFac
   
+  print*,'SclFac: ', sclFac
 end subroutine pl_square_grid
 !***********************************************************************************************************************************
   
