280d187dc01d6784edab7cb21e0a1d809dad2e91
MvdS <git@vandersluys.nl>
2016-08-06 17:19:35 +0200

    Add licence

--- make_schedule.f90
+++ make_schedule.f90
@@ -1,3 +1,18 @@
+!  Copyright (c) 2016  Marc van der Sluys - han.vandersluys.nl
+!   
+!  This file is part of the RT Schedule package, 
+!  see: http://rtschedule.sourceforge.net/
+!   
+!  This is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published
+!  by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+!  
+!  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+!  
+!  You should have received a copy of the GNU General Public License along with this code.  If not, see 
+!  <http://www.gnu.org/licenses/>.
+
+
 !***********************************************************************************************************************************
 subroutine make_schedule(sched, np,time, name, ti,ci,di,pi, load, fileBaseName)
   use SUFR_kinds, only: double
--- schedule.f90
+++ schedule.f90
@@ -1,5 +1,22 @@
 !> \file schedule.f90  Main routines for schedule
 
+
+
+!  Copyright (c) 2016  Marc van der Sluys - han.vandersluys.nl
+!   
+!  This file is part of the RT Schedule package, 
+!  see: http://rtschedule.sourceforge.net/
+!   
+!  This is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published
+!  by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+!  
+!  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+!  
+!  You should have received a copy of the GNU General Public License along with this code.  If not, see 
+!  <http://www.gnu.org/licenses/>.
+
+
 !***********************************************************************************************************************************
 !> \brief  Module to share plot settings
 module settings
